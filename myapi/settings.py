3a46daf4f45575f3debc2748c62554c679309eff
Hyeon Jin Jeong <121111644+MysteryDevi@users.noreply.github.com>
2023-03-07 23:02:10 +0900

    Update settings.py

--- myapi/settings.py
+++ myapi/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'whitenoise.middlewate.WhiteNoiseMiddleware',
+    'whitenoise.middlewae.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "myapi.urls"
